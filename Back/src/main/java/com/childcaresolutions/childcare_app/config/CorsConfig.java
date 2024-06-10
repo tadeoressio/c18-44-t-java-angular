a8a40bdb673565f613078d7e13fd9cc9ee4f548f
Alexis Alejandro Calderon <alexiscalderon202@gmail.com>
2024-06-09 21:32:28 -0300

    Cors-Solution

--- Back/src/main/java/com/childcaresolutions/childcare_app/config/CorsConfig.java
+++ Back/src/main/java/com/childcaresolutions/childcare_app/config/CorsConfig.java
@@ -1,12 +1,14 @@
 package com.childcaresolutions.childcare_app.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 
+@Configuration
 public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
