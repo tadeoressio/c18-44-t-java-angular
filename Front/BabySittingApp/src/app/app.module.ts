93e3cbe28575ff7eba907d0fd67e9e25c5833afe
tadeoressio <tadeo.ressio@gmail.com>
2024-06-12 13:46:27 -0300

    delete unecessary folders and primeng

--- Front/BabySittingApp/src/app/app.module.ts
+++ Front/BabySittingApp/src/app/app.module.ts
@@ -9,19 +9,9 @@ import {HttpClientModule} from '@angular/common/http';
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
-import { CascadeSelectModule } from 'primeng/cascadeselect';
-import { MenuModule } from 'primeng/menu';
-import { BadgeModule } from 'primeng/badge';
-import { MenubarModule } from 'primeng/menubar';
-import { CardModule } from 'primeng/card';
 
 import { HomePageComponent } from './Features/home-page/home-page.component';
 import { NavBarComponent } from './Shared/nav-bar/nav-bar.component';
-import { LoggedMenuComponent } from './Shared/logged-menu/logged-menu.component';
-import { LoginMenuComponent } from './Shared/login-menu/login-menu.component';
-import { NotMenuComponent } from './Shared/not-menu/not-menu.component';
-import { LikMenuComponent } from './Shared/lik-menu/lik-menu.component';
 import { FirstQuestionRegisterComponent } from './Features/first-question-register/first-question-register.component';
 import { RegisterDadsComponent } from './Features/register-dads/register-dads.component';
 import { RegisterBabySittersComponent } from './Features/register-baby-sitters/register-baby-sitters.component';
@@ -59,18 +49,8 @@ import { FooterComponent } from './Shared/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ButtonModule,
     BrowserAnimationsModule,
-    CascadeSelectModule,
     FormsModule,
-    MenuModule,
-    LoggedMenuComponent,
-    LoginMenuComponent,
-    NotMenuComponent,
-    BadgeModule,
-    LikMenuComponent,
-    MenubarModule,
-    CardModule,
     NgbModule,
   ],
   providers: [],
